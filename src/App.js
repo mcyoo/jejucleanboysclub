e10228623a21b9d323b3e28099fd6ff57f6238a1
mcyoo <64882430+mcyoo@users.noreply.github.com>
2020-11-20 13:20:23 +0900

    jejucleanboysclub#8

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ReactTypingEffect from "react-typing-effect";
 import linkicon1 from "./linkicon1.png";
 import linkicon2 from "./linkicon2.png";
 import linkicon3 from "./linkicon3.png";
+import CountUp from "react-countup";
 
 class App extends React.Component {
   state = {
@@ -40,32 +41,43 @@ class App extends React.Component {
     const settings = {
       dots: true,
       infinite: true,
-      speed: 400,
+      speed: 200,
       slidesToShow: 1,
       slidesToScroll: 1,
       appendDots: (dots) => (
         <div
           style={{
             position: "fixed",
-            padding: "30px",
+            padding: "25px",
             backgroundColor: "white",
             textAlign: "center",
+            borderTop: "1px solid #e2e8f0",
           }}
         >
-          <div style={{}}> {dots} </div>
+          <div style={{ flex: 1 }}> {dots} </div>
         </div>
       ),
       customPaging: (i) => {
         if (i === 0) {
           return (
-            <img width="500" height="500" alt="linkicon1" src={linkicon1} />
+            <div style={{ flex: 1, width: "3em" }}>
+              <img alt="linkicon1" src={linkicon1} />
+            </div>
           );
         }
         if (i === 1) {
-          return <img width="500" height="500" src={linkicon2} />;
+          return (
+            <div style={{ flex: 1, width: "3em" }}>
+              <img alt="linkicon2" src={linkicon2} />
+            </div>
+          );
         }
         if (i === 2) {
-          return <img width="500" height="500" src={linkicon3} />;
+          return (
+            <div style={{ flex: 1, width: "3em" }}>
+              <img alt="linkicon3" src={linkicon3} />
+            </div>
+          );
         }
       },
     };
@@ -78,35 +90,50 @@ class App extends React.Component {
         ) : (
           <div className="">
             <Slider {...settings}>
-              <div className="">
+              <div className="w-full">
                 <div className="text-5xl lg:text-5xl sm:text-xl lg:mx-32 sm:mx-8">
-                  <h3>우리는</h3>
-                  <ReactTypingEffect
-                    cursorRenderer={(cursor) => <h1>{cursor}</h1>}
-                    displayTextRenderer={(text, j) => {
-                      return (
-                        <h2>
-                          {text.split("").map((char, i) => {
-                            const key = `${i}`;
-                            return <span key={key}>{char}</span>;
-                          })}
-                        </h2>
-                      );
-                    }}
-                    text={[
-                      "쓰레기를 줍습니다.",
-                      "지역사회에 기여합니다.",
-                      "jejucleanboysclub입니다.",
-                    ]}
-                    speed={120}
-                    typingDelay={1200}
-                    eraseDelay={1500}
-                    eraseSpeed={100}
-                  />
+                  <div className="mb-10 lg:mb-10 sm:mb-10">
+                    <h3>우리는</h3>
+                    <ReactTypingEffect
+                      cursorRenderer={(cursor) => <h1>{cursor}</h1>}
+                      displayTextRenderer={(text, j) => {
+                        return (
+                          <h2>
+                            {text.split("").map((char, i) => {
+                              const key = `${i}`;
+                              return <span key={key}>{char}</span>;
+                            })}
+                          </h2>
+                        );
+                      }}
+                      text={[
+                        "쓰레기를 줍습니다.",
+                        "지역사회에 기여합니다.",
+                        "jejucleanboysclub입니다.",
+                      ]}
+                      speed={120}
+                      typingDelay={1200}
+                      eraseDelay={1500}
+                      eraseSpeed={100}
+                    />
+                  </div>
+                  <div className="border-t border-gray-400">
+                    <div className="text-xl lg:text-2xl sm:text-lg">
+                      <div className="mr-4">
+                        <br></br>누적 활동횟수 <CountUp end={30} delay={1} />
+                      </div>
+                    </div>
+
+                    <div className="text-xl lg:text-2xl sm:text-lg">
+                      <div className="mr-4">
+                        <br></br>참여인원 <CountUp end={30} delay={1} />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="text-center text-lg">
-                카카오 맵
+              <div className="text-center text-lg w-full">
+                작전 지역
                 <div
                   onMouseDown={(e) => {
                     e.preventDefault();
@@ -116,12 +143,12 @@ class App extends React.Component {
                     e.preventDefault();
                     e.stopPropagation();
                   }}
-                  className="mx-32 lg:mx-32 sm:mx-10"
+                  className="mx-32 lg:mx-64 sm:mx-12"
                 >
                   <KakaoMap />
                 </div>
               </div>
-              <div className="text-center text-lg">
+              <div className="text-center text-lg w-full">
                 <h3>명예의 전당</h3>
               </div>
             </Slider>
